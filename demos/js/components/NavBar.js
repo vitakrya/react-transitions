d784791a799a30eb71b757e5f515d76458c30ad9
Steven Hall <hall.steven.g@gmail.com>
2015-07-19 11:06:16 -0700

    add dropdown to nav

--- demos/js/components/NavBar.js
+++ demos/js/components/NavBar.js
@@ -10,6 +10,12 @@ export let NavBar = React.createClass({
     };
   },
 
+  handleBlur: function() {
+    this.setState({
+      menuOpen: false
+    });
+  },
+
   handleClick: function() {
     this.setState({
       menuOpen: !this.state.menuOpen
@@ -17,30 +23,6 @@ export let NavBar = React.createClass({
   },
 
   render: function() {
-    let menu;
-
-    if (this.state.menuOpen) {
-      menu = (
-        <ul className="dropdown-menu animated userinfo arrow">
-          <li className="userlinks">
-          <ul className="dropdown-menu">
-            <li><a href="#">Edit Profile <i className="pull-right glyphicon glyphicon-pencil"></i></a></li>
-            <li><a href="#">Account <i className="pull-right glyphicon glyphicon-user"></i></a></li>
-            <li><a href="#">Settings <i className="pull-right glyphicon glyphicon-cog"></i></a></li>
-            <li className="divider"></li>
-            <li><a href="#">Earnings <i className="pull-right glyphicon glyphicon-stats"></i></a></li>
-            <li><a href="#">Statement <i className="pull-right glyphicon glyphicon-list-alt"></i></a></li>
-            <li><a href="#">Withdrawals <i className="pull-right glyphicon glyphicon-credit-card"></i></a></li>
-            <li className="divider"></li>
-            <li><a href="#" className="text-right">Sign Out</a></li>
-          </ul>
-          </li>
-        </ul>
-      );
-    } else {
-      menu = (<div></div>);
-    }
-
     return (
       <header id="topnav" className="navbar navbar-fixed-top" role="banner">
         <a id="leftmenu-trigger"></a>
@@ -50,10 +32,29 @@ export let NavBar = React.createClass({
         </div>
         <ul className="nav navbar-nav pull-right toolbar">
             <li className={`dropdown ${this.state.menuOpen === true ? "open": ""}`} dropdown="">
-              <a href="#" className="dropdown-toggle username" onClick={this.handleClick}>
+              <a
+                href="#"
+                className="dropdown-toggle username"
+                onClick={this.handleClick}
+                onBlur={this.handleBlur}
+              >
                 <img src="http://placehold.it/400&amp;text=Placeholder" alt="Dangerfield" />
               </a>
-              {menu}
+              <ul className="dropdown-menu animated userinfo arrow">
+                <li className="userlinks">
+                <ul className="dropdown-menu">
+                  <li><a href="#">Edit Profile <i className="pull-right glyphicon glyphicon-pencil"></i></a></li>
+                  <li><a href="#">Account <i className="pull-right glyphicon glyphicon-user"></i></a></li>
+                  <li><a href="#">Settings <i className="pull-right glyphicon glyphicon-cog"></i></a></li>
+                  <li className="divider"></li>
+                  <li><a href="#">Earnings <i className="pull-right glyphicon glyphicon-stats"></i></a></li>
+                  <li><a href="#">Statement <i className="pull-right glyphicon glyphicon-list-alt"></i></a></li>
+                  <li><a href="#">Withdrawals <i className="pull-right glyphicon glyphicon-credit-card"></i></a></li>
+                  <li className="divider"></li>
+                  <li><a href="#" className="text-right">Sign Out</a></li>
+                </ul>
+                </li>
+              </ul>
             </li>
           </ul>
       </header>
